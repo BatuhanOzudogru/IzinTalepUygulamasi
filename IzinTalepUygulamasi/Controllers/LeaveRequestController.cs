08b36b8949c2c8f3f8ceeac531a9e8c3debf6d2f
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2025-10-12 01:31:28 +0300

    feat(employee): LeaveRequest kayıt ekleme ve listeleme özellikleri eklendi.

---
+++ IzinTalepUygulamasi/Controllers/LeaveRequestController.cs
@@ -0,0 +1,78 @@
+﻿using IzinTalepUygulamasi.Data;
+using IzinTalepUygulamasi.Models;
+using IzinTalepUygulamasi.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace IzinTalepUygulamasi.Controllers
+{
+    [Authorize]
+    public class LeaveRequestController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public LeaveRequestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(string statusFilter) 
+        {
+            int userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            var userLeaveRequestsQuery = _context.LeaveRequests
+                                                 .Where(lr => lr.RequestingEmployeeId == userId);
+
+            if (!string.IsNullOrEmpty(statusFilter))
+            {
+                RequestStatus status = (RequestStatus)Enum.Parse(typeof(RequestStatus), statusFilter);
+                userLeaveRequestsQuery = userLeaveRequestsQuery.Where(lr => lr.Status == status);
+            }
+
+            var leaveRequests = await userLeaveRequestsQuery.OrderBy(lr => lr.RequestDate).ToListAsync();
+
+            ViewBag.StatusFilter = statusFilter;
+
+            return View(leaveRequests);
+        }
+
+        [Authorize(Roles = "Employee")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = new LeaveRequestCreateViewModel();
+            return View(model);
+        }
+
+        [Authorize(Roles = "Employee")]
+        [HttpPost]
+        public async Task<IActionResult> Create(LeaveRequestCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var requestingEmployeeId = int.Parse(userId);
+
+                var leaveRequest = new LeaveRequest
+                {
+                    RequestingEmployeeId = requestingEmployeeId,
+                    LeaveType = model.LeaveType,
+                    StartDate = model.StartDate,
+                    EndDate = model.EndDate,
+                    Reason = model.Reason,
+                    RequestDate = DateTime.Now,
+                    Status = RequestStatus.PENDING,
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = User.Identity.Name
+                };
+
+                await _context.LeaveRequests.AddAsync(leaveRequest);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ IzinTalepUygulamasi/Models/ViewModels/LeaveRequestCreateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IzinTalepUygulamasi.Models.ViewModels
+{
+    public class LeaveRequestCreateViewModel
+    {
+        [Required]
+        public LeaveType LeaveType { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; } = DateTime.Now;
+
+        [MaxLength(500)]
+        public string? Reason { get; set; }
+    }
+}
