15b023d5f6a4d2e4e586d5f88250293e365e4837
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2025-10-13 22:58:10 +0300

    feat(manager): Yöneticinin onay bekleyen talepleri listeleme sayfası eklendi

---
+++ IzinTalepUygulamasi/Controllers/ManagerController.cs
@@ -0,0 +1,33 @@
+﻿using IzinTalepUygulamasi.Data;
+using IzinTalepUygulamasi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IzinTalepUygulamasi.Controllers
+{
+    [Authorize(Roles = "Manager")]
+    public class ManagerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ManagerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var pendingRequests = await _context.LeaveRequests
+                .Include(lr => lr.RequestingEmployee)
+                .Where(lr => lr.Status == RequestStatus.PENDING)
+                .OrderBy(lr => lr.RequestDate)
+                .ToListAsync();
+
+            return View(pendingRequests);
+        }
+        public IActionResult Report()
+        {
+            return View();
+        }
+    }
+}
